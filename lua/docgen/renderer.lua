3886467ab26e47cdf25af75d487c46b129f7dbc6
Simon Hauser <Simon-Hauser@outlook.de>
2021-03-02 14:26:04 +0100

    Add <pre> and <br>

--- lua/docgen/renderer.lua
+++ lua/docgen/renderer.lua
@@ -9,7 +9,8 @@ function Text:new()
     paragraphs = {},
     itemizes = {},
     enumerates = {},
-    order = {}
+    ignores = {},
+    order = {},
   }, Text)
 end
 
@@ -17,6 +18,8 @@ local states = setmetatable({
   NEWLINE = 'newline',
   ITEMIZE = 'itemize',
   ENUMERATE = 'enumerate',
+  IGNORE = 'ignore',
+  ENDIGNORE = 'endignore',
   PARAGRAPH = 'paragraph'
 }, {
   __index = function(_, k)
@@ -34,12 +37,16 @@ local interpret_state = function(line)
     return states.ITEMIZE
   elseif vim.trim(line):match('^[0-9]') then
     return states.ENUMERATE
+  elseif line == '<pre>' then
+    return states.IGNORE
+  elseif line == '</pre>' then
+    return states.ENDIGNORE
   else
     return states.PARAGRAPH
   end
 end
 
-local run_state = {
+local dispatch_state = {
   [states.PARAGRAPH] = function(self, line) return self:add_to_paragraph(line) end,
   [states.NEWLINE] = function(self) self:start_new_paragraph() end,
   [states.ITEMIZE] = function(self, line) return self:add_to_itemize(line) end,
@@ -57,75 +64,125 @@ local append = function(l, r)
   end
 end
 
+function Text:error()
+  error(
+    string.format('Error while rendering things. Order: %s, paragraphs: %s, itemizes: %s, enumerates: %s, ignores: %s',
+      vim.inspect(self.order),
+      vim.inspect(self.paragraphs),
+      vim.inspect(self.itemizes),
+      vim.inspect(self.enumerates),
+      vim.inspect(self.ignores)
+    )
+  )
+end
+
 function Text:handle_line(line)
   local new_state = interpret_state(line)
+
   if new_state == self.state then
-    run_state[new_state](self, line)
+    -- Happens when we didn't have a change in state
+    dispatch_state[new_state](self, line)
+  elseif new_state == states.IGNORE then
+    -- Start new Ignore block
+    self:start_new_ignore()
+    self.state = new_state
+  elseif self.state == states.IGNORE and new_state ~= states.ENDIGNORE then
+    -- Write lines to ignore block
+    self:add_to_ignore(line)
+  elseif self.state == states.IGNORE and new_state == states.ENDIGNORE then
+    -- End ignore block
+    self.state = nil
   elseif self.state == states.NEWLINE and new_state == states.PARAGRAPH then
-    self:start_new_paragraph()
-    run_state[new_state](self, line)
-    table.insert(self.order, new_state)
-    self.state = new_state
-  elseif new_state == states.ITEMIZE then
-    self:start_new_itemize()
-    run_state[new_state](self, line)
-    table.insert(self.order, new_state)
-    self.state = new_state
-  elseif self.state == states.ITEMIZE and new_state == states.PARAGRAPH then
-    self:append_to_itemize(line)
-  elseif self.state == states.ITEMIZE and new_state == states.NEWLINE then
-    table.insert(self.order, states.PARAGRAPH)
-    run_state[new_state](self, line)
-    self.state = new_state
-  else
-    if new_state == states.NEWLINE then
-      table.insert(self.order, self.state)
-    else
+    -- From newline into new Paragraph
+    if self.order[table.getn(self.order)] == states.IGNORE then
+      -- If prev state was IGNORE add a newline on top
       table.insert(self.order, new_state)
     end
-    run_state[new_state](self, line)
-
+    self:start_new_paragraph(line)
     self.state = new_state
+  elseif new_state == states.ITEMIZE then
+    -- Start new Itemize block
+    self:start_new_itemize(line)
+    self.state = new_state
+  elseif self.state == states.ITEMIZE and new_state == states.PARAGRAPH then
+    -- If we have a new paragraph in an itemize (part of the previous itemize)
+    self:append_to_itemize(line)
+  elseif self.state == nil and new_state == states.PARAGRAPH then
+    -- Start a new paragraph
+    self:start_new_paragraph(line)
+    self.state = new_state
+  elseif new_state == states.NEWLINE and (self.state == states.ITEMIZE or
+         self.state == states.PARAGRAPH or
+         self.state == nil) then
+    -- After a itemize have a empty line or
+    -- Insert newline when Paragraph or
+    -- Newline after a prev block was closed (pre)
+    self:start_new_paragraph()
+    self.state = new_state
+  else
+    self:error()
   end
 end
 
 function Text:iter()
   local i = 0
-  local i_s = { paragraph = 0, itemize = 0, enumerate = 0 }
+  local i_s = { paragraph = 0, itemize = 0, enumerate = 0, ignore = 0 }
   local n = table.getn(self.order)
   return function()
     i = i + 1
     if i <= n then
       i_s[self.order[i]] = i_s[self.order[i]] + 1
       if self.order[i] == states.PARAGRAPH then
-        return self.paragraphs[i_s[self.order[i]]]
+        return self.order[i], self.paragraphs[i_s[self.order[i]]]
       elseif self.order[i] == states.ITEMIZE then
-        return self.itemizes[i_s[self.order[i]]]
+        return self.order[i], self.itemizes[i_s[self.order[i]]]
       elseif self.order[i] == states.ENUMERATE then
-        return self.enumerates[i_s[self.order[i]]]
+        return self.order[i], self.enumerates[i_s[self.order[i]]]
+      elseif self.order[i] == states.IGNORE then
+        return self.order[i], self.ignores[i_s[self.order[i]]]
       else
-        error('Error in render engine')
+        self:error()
       end
     end
   end
 end
 
-function Text:start_new_paragraph()
+function Text:start_new_paragraph(line)
   table.insert(self.paragraphs, "")
+  table.insert(self.order, states.PARAGRAPH)
+  if line then
+    self:add_to_paragraph(line)
+  end
 end
 
-function Text:start_new_itemize()
+function Text:start_new_itemize(line)
   table.insert(self.itemizes, { })
+  table.insert(self.order, states.ITEMIZE)
+  if line then
+    self:add_to_itemize(line)
+  end
+end
+
+function Text:start_new_ignore()
+  table.insert(self.ignores, { })
+  table.insert(self.order, states.IGNORE)
 end
 
 function Text:add_to_paragraph(str)
-  if table.getn(self.paragraphs) == 0 then self:start_new_paragraph() end
-  self.paragraphs[#self.paragraphs] = append(self.paragraphs[#self.paragraphs], str)
+  if str:match('<br>$') then
+    self.paragraphs[#self.paragraphs] = append(self.paragraphs[#self.paragraphs], str:gsub('<br>$', ''))
+    self:start_new_paragraph()
+  else
+    self.paragraphs[#self.paragraphs] = append(self.paragraphs[#self.paragraphs], str)
+  end
 end
 
 function Text:add_to_itemize(str)
-  if table.getn(self.itemizes) == 0 then self:start_new_itemize() end
   table.insert(self.itemizes[#self.itemizes], str)
+end
+
+function Text:add_to_ignore(str)
+  table.insert(self.ignores[#self.ignores], str)
 end
 
 function Text:append_to_itemize(str)
@@ -155,7 +212,6 @@ m.render = function(input, prefix, width)
     end
   end
 
-  local default_prefix_width = #prefix
   local output = {}
 
   local handle = function(string, additional_prefix)
@@ -175,16 +231,23 @@ m.render = function(input, prefix, width)
     table.insert(output, line)
   end
 
-  for paragraph in text:iter() do
-    if type(paragraph) == 'string' then
+  for type, paragraph in text:iter() do
+    if type == states.PARAGRAPH then
       handle(paragraph)
-    else
+    elseif type == states.ITEMIZE then
       for _, str in ipairs(paragraph) do
         local str_start, _ = str:find('-')
         local base = string.rep(' ', (str_start - 1))
         local additional_prefix = { base, base .. '  ' }
         handle(str, additional_prefix)
       end
+    elseif type == states.IGNORE then
+      for _, str in ipairs(paragraph) do
+        local construct = (prefix .. str):gsub('%s*$', '')
+        table.insert(output, construct)
+      end
+    else
+      text:error()
     end
   end
   return table.concat(output, '\n')
--- lua/tests/docgen/renderer_spec.lua
+++ lua/tests/docgen/renderer_spec.lua
@@ -1,6 +1,29 @@
 local eq = assert.are.same
 local renderer = require('docgen.renderer')
 
+local dedent = function(str, leave_indent)
+  -- find minimum common indent across lines
+  local indent = nil
+  for line in str:gmatch('[^\n]+') do
+    local line_indent = line:match('^%s+') or ''
+    if indent == nil or #line_indent < #indent then
+      indent = line_indent
+    end
+  end
+  if indent == nil or #indent == 0 then
+    -- no minimum common indent
+    return str
+  end
+  local left_indent = (' '):rep(leave_indent or 0)
+  -- create a pattern for the indent
+  indent = indent:gsub('%s', '[ \t]')
+  -- strip it from the first line
+  str = str:gsub('^'..indent, left_indent)
+  -- strip it from the remaining lines
+  str = str:gsub('[\n]'..indent, '\n' .. left_indent)
+  return str
+end
+
 describe('renderer', function()
   describe('without prefix', function()
     it('should handle short lines', function()
@@ -9,6 +32,10 @@ describe('renderer', function()
 
     it('should handle short lines with multiple entires', function()
       eq('Hello World', renderer.render({ 'Hello' , 'World' }, '', 80))
+    end)
+
+    it('should allow manually breakage at the end of line', function()
+      eq('Hello\nWorld', renderer.render({ 'Hello<br>' , 'World' }, '', 80))
     end)
 
     it('should wrap line that exceed width', function()
@@ -35,6 +62,10 @@ describe('renderer', function()
 
     it('should handle short lines with multiple entires', function()
       eq('    Hello World', renderer.render({ 'Hello' , 'World' }, '    ', 80))
+    end)
+
+    it('should allow manually breakage at the end of line', function()
+      eq('    Hello\n    World', renderer.render({ 'Hello<br>' , 'World' }, '    ', 80))
     end)
 
     it('should wrap line that exceed width', function()
@@ -82,21 +113,22 @@ describe('renderer', function()
     end)
 
     it('works with longer nested itemize', function()
-      eq([[- item one with
-  some
-  additional
-  context
-  - item one.one
-    with more
-    context
-    - item
-      one.one.one
-      with even
-      more
-      context
-  - item one.two
-    no context
-- item two]],
+      eq(dedent([[
+        - item one with
+          some
+          additional
+          context
+          - item one.one
+            with more
+            context
+            - item
+              one.one.one
+              with even
+              more
+              context
+          - item one.two
+            no context
+        - item two]]),
         renderer.render({
           '- item one with some additional context',
           '  - item one.one with more context',
@@ -166,82 +198,152 @@ describe('renderer', function()
     end)
   end)
 
-  it('can handle a text with paragraphes and itemize', function()
-    local input = vim.trim[[
-This is the first paragraph and describes a cool looking function. This string is longer than 80 so
-the renderer should wrap it.
-If not this would be pretty bad.
+  describe('combination', function()
+    it('can handle a text with paragraphes and itemize', function()
+      local input = dedent[[
+        This is the first paragraph and describes a cool looking function. This string is longer than 80 so
+        the renderer should wrap it.
+        If not this would be pretty bad.
 
-This is a new and short paragraph and the header of itemize:
-- This is the first item
-- This is the second item and is way longer than the first item which means it has to be wrapped. I hope
-the renderer does this for me
-- This is a third short item
-    ]]
+        This is a new and short paragraph and the header of itemize:
+        - This is the first item
+        - This is the second item and is way longer than the first item which means it has to be wrapped. I hope
+        the renderer does this for me
+        - This is a third short item]]
 
-    local expected = [[This is the first paragraph and describes a cool looking function. This string
-is longer than 80 so the renderer should wrap it. If not this would be pretty
-bad.
+      local expected = dedent[[
+        This is the first paragraph and describes a cool looking function. This string
+        is longer than 80 so the renderer should wrap it. If not this would be pretty
+        bad.
 
-This is a new and short paragraph and the header of itemize:
-- This is the first item
-- This is the second item and is way longer than the first item which means it
-  has to be wrapped. I hope the renderer does this for me
-- This is a third short item]]
-    eq(expected, renderer.render(vim.split(input, '\n'), '', 80))
+        This is a new and short paragraph and the header of itemize:
+        - This is the first item
+        - This is the second item and is way longer than the first item which means it
+          has to be wrapped. I hope the renderer does this for me
+        - This is a third short item]]
+      eq(expected, renderer.render(vim.split(input, '\n'), '', 80))
+    end)
+
+    it('can switch between itemize and paragraph', function()
+      local input = dedent[[
+        This is the first paragraph and describes a cool looking function. This string is longer than 80 so
+        the renderer should wrap it.
+        If not this would be pretty bad.
+
+        - This is the first item
+        - This is the second item and is way longer than the first item which means it has to be wrapped. I hope
+        the renderer does this for me
+        - This is a third short item
+
+        New Paragraph. Please be shown
+
+        - New itemize]]
+
+      local expected = dedent[[
+        This is the first paragraph and describes a cool looking function. This string
+        is longer than 80 so the renderer should wrap it. If not this would be pretty
+        bad.
+
+        - This is the first item
+        - This is the second item and is way longer than the first item which means it
+          has to be wrapped. I hope the renderer does this for me
+        - This is a third short item
+
+        New Paragraph. Please be shown
+
+        - New itemize]]
+      eq(expected, renderer.render(vim.split(input, '\n'), '', 80))
+    end)
+
+    it('More fluid changes between paragraph and itemize', function()
+      local input = dedent[[
+        Examples of wrappers are:
+          - `new_buffer_previewer`
+          - `new_termopen_previewer`
+
+        To create a new table do following:
+          - `local new_previewer = Previewer:new(opts)`
+
+        What `:new` expects is listed below]]
+
+      local expected = dedent[[
+        Examples of wrappers are:
+          - `new_buffer_previewer`
+          - `new_termopen_previewer`
+
+        To create a new table do following:
+          - `local new_previewer = Previewer:new(opts)`
+
+        What `:new` expects is listed below]]
+      eq(expected, renderer.render(vim.split(input, '\n'), '', 80))
+    end)
   end)
 
-  it('can switch between itemize and paragraph', function()
-    local input = vim.trim[[
-This is the first paragraph and describes a cool looking function. This string is longer than 80 so
-the renderer should wrap it.
-If not this would be pretty bad.
+  describe('pre without prefix', function()
+    it('can ignore simple blocks', function()
+      eq('Hello World', renderer.render({'<pre>', 'Hello World', '</pre>' }, '', 80))
+    end)
 
-- This is the first item
-- This is the second item and is way longer than the first item which means it has to be wrapped. I hope
-the renderer does this for me
-- This is a third short item
+    it('can ignore complex blocks', function()
+      eq('Steps\n Hello\n  World', renderer.render({'<pre>', 'Steps', ' Hello', '  World', '</pre>' }, '', 80))
+    end)
 
-New Paragraph. Please be shown
+    it('combination with Paragraph', function()
+      eq(dedent[[
+        This is a long paragraph that will be wrapped after that we expect a pre block
+        that will ignore stuff
 
-- New itemize
-    ]]
+        Steps
+         Hello
+          World
 
-    local expected = [[This is the first paragraph and describes a cool looking function. This string
-is longer than 80 so the renderer should wrap it. If not this would be pretty
-bad.
-
-- This is the first item
-- This is the second item and is way longer than the first item which means it
-  has to be wrapped. I hope the renderer does this for me
-- This is a third short item
-
-New Paragraph. Please be shown
-
-- New itemize]]
-    eq(expected, renderer.render(vim.split(input, '\n'), '', 80))
+        End paragraph just for the LULW s. Please just work.]],
+        renderer.render({
+          'This is a long paragraph that will be wrapped after that we expect a pre block that will ignore stuff',
+          '',
+          '<pre>',
+          'Steps',
+          ' Hello',
+          '  World',
+          '</pre>',
+          '',
+          'End paragraph just for the LULW s. Please just work.',
+        }, '', 80))
+    end)
   end)
 
-  it('More fluid changes between paragraph and itemize', function()
-    local input = [[
-Examples of wrappers are:
-  - `new_buffer_previewer`
-  - `new_termopen_previewer`
+  describe('pre with prefix', function()
+    it('can ignore simple blocks', function()
+      eq('    Hello World', renderer.render({'<pre>', 'Hello World', '</pre>' }, '    ', 80))
+    end)
 
-To create a new table do following:
-  - `local new_previewer = Previewer:new(opts)`
+    it('can ignore complex blocks', function()
+      eq([[    Steps
+     Hello
+      World]], renderer.render({'<pre>', 'Steps', ' Hello', '  World', '</pre>' }, '    ', 80))
+    end)
 
-What `:new` expects is listed below]]
+    it('combination with Paragraph', function()
+      eq([[    This is a long paragraph that will be wrapped after that we expect a pre
+    block that will ignore stuff
 
-    local expected = [[
-Examples of wrappers are:
-  - `new_buffer_previewer`
-  - `new_termopen_previewer`
+    Steps
+     Hello
+      World
 
-To create a new table do following:
-  - `local new_previewer = Previewer:new(opts)`
-
-What `:new` expects is listed below]]
-    eq(expected, renderer.render(vim.split(input, '\n'), '', 80))
+    End paragraph just for the LULW s. Please just work.]], renderer.render(
+        {
+          'This is a long paragraph that will be wrapped after that we expect a pre block that will ignore stuff',
+          '',
+          '<pre>',
+          'Steps',
+          ' Hello',
+          '  World',
+          '</pre>',
+          '',
+          'End paragraph just for the LULW s. Please just work.',
+        }, '    ', 80))
+    end)
   end)
+
 end)
