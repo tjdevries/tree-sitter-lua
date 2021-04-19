d1d57d5fea1fcdcd87c8e2968a27a4fa4d739516
TJ DeVries <devries.timothyj@gmail.com>
2021-04-18 23:32:10 -0400

    wip: continue stuff

--- queries/lua/injections.scm
+++ queries/lua/injections.scm
@@ -8,3 +8,10 @@
 
  (#eq? @_prefix_2 "cdef")
 )
+
+(
+ (function_call
+  prefix: (identifier) @_prefix_1
+  args: (string_argument) @lua)
+
+ (#eq? @_prefix_1 "exec_lua"))
---
+++ queries/lua/locals.scm
@@ -0,0 +1,19 @@
+;; Variable and field declarations
+((variable_declarator
+   (identifier) @definition.var))
+
+;; Parameters
+(parameter_list (identifier) @definition.parameter)
+
+
+;; Scopes
+[
+  (program)
+  (function_statement)
+  (function_start)
+  (if_statement)
+  (for_statement)
+  (repeat_statement)
+  (while_statement)
+  (do_statement)
+] @scope
