db1478e96cbe8b0272e87567510d84363f07767b
TJ DeVries <devries.timothyj@gmail.com>
2021-11-01 14:42:52 -0400

    fix: remove my custom graphql query

--- queries/lua/injections.scm
+++ queries/lua/injections.scm
@@ -10,21 +10,6 @@
  (#offset! @c 0 2 0 -2)
 )
 
-(
- (function_call
-  prefix: (identifier) @_prefix_1
-  args: (string_argument) @lua)
-
- (#eq? @_prefix_1 "exec_lua"))
-
-(
-  (variable_declaration
-   name: (variable_declarator) @_id
-   value: (string) @graphql)
-
-  (#match? @_id ".*GraphQL")
-)
-
 (comment) @comment
 
 (
